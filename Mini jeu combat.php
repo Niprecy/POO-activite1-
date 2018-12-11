30eb66ad2827905a59de6d9ff0e25ba9c0b58a33
Cyprien Bleuze <cyprienbleuze28@gmail.com>
2018-12-11 10:55:28 +0100

    je copie le fichier Mini Jeu Combat dans Git Hub

---
+++ Mini jeu combat.php
@@ -0,0 +1,21 @@
+<?php
+class Personnage
+{
+	//~ private $_id;
+	//~ private $_nom;
+	//~ private $_forcePerso;
+	//~ private $_degats;
+	//~ private $_niveau;
+	//~ private $_experience;
+	
+	
+	private $_id,
+			$_nom,
+			$_forcePerso;
+			$_degats;
+			$_niveau;
+			$_experience;
+			
+	public
+}
+
